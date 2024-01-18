6e23dfdac5af6d17eeeda2852bd6f633c77ab73c
golfomania <ml.loeffler@gmx.de>
2024-01-18 20:30:50 +0100

    UI finished at 45min

--- crud_app/src/components/Authenticate.svelte
+++ crud_app/src/components/Authenticate.svelte
@@ -1,16 +1,60 @@
+<script>
+	let email = '';
+	let password = '';
+	let confirmPassword = '';
+	let error = false;
+	let register = false;
+
+	function handleAuthenticate() {
+		if (!email || !password || (register && !confirmPassword)) {
+			alert('Please fill out all fields');
+			error = true;
+			return;
+		}
+	}
+
+	const handleRegister = () => {
+		register = !register;
+	};
+</script>
+
 <div class="authContainer">
 	<form>
-		<h1>Login</h1>
+		<h1>{register ? 'Register' : 'Login'}</h1>
+		{#if error}
+			<p class="error">Given information not correct</p>
+		{/if}
 		<label for="">
-			<input type="email" placeholder="E-Mail" />
+			<p class={email ? ' above' : ' center'}>E-Mail</p>
+			<input bind:value={email} type="email" placeholder="E-Mail" />
 		</label>
 		<label for="">
-			<input type="password" placeholder="Password" />
-		</label><label for="">
-			<input type="password" placeholder="Confirm Password" />
+			<p class={password ? ' above' : ' center'}>Password</p>
+			<input bind:value={password} type="password" placeholder="Password" />
 		</label>
+		{#if register}
+			<label for="">
+				<p class={confirmPassword ? ' above' : ' center'}>Confirm Password</p>
+				<input bind:value={confirmPassword} type="password" placeholder="Confirm Password" />
+			</label>
+		{/if}
+
 		<button type="button">Submit</button>
 	</form>
+	<div class="options">
+		<p>Or</p>
+		{#if register}
+			<div>
+				<p>Already have an account?</p>
+				<p on:click={handleRegister} on:keydown={() => {}}>Login</p>
+			</div>
+		{:else}
+			<div>
+				<p>Don't have an account?</p>
+				<p on:click={handleRegister} on:keydown={() => {}}>Register</p>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <style>
@@ -22,10 +66,15 @@
 		flex: 1;
 		padding: 24px;
 	}
+
 	form {
 		display: flex;
 		flex-direction: column;
-		gap: 8px;
+		gap: 14px;
+	}
+
+	form,
+	.options {
 		width: 400px;
 		max-width: 100%;
 		margin: 0 auto;
@@ -33,20 +82,6 @@
 
 	form input {
 		width: 100%;
-		padding: 14px;
-		border-radius: 4px;
-		border: none;
-		background: transparent;
-		color: white;
-	}
-	form input:focus {
-		outline: none;
-	}
-
-	form label {
-		position: relative;
-		border: 1px solid darkblue;
-		border-radius: 5px;
 	}
 
 	h1 {
@@ -54,17 +89,134 @@
 		font-size: 3rem;
 	}
 
-	button {
-		background: darkblue;
+	form label {
+		position: relative;
+		border: 1px solid navy;
+		border-radius: 5px;
+	}
+
+	form input {
+		border: none;
+		background: transparent;
+		color: white;
+		padding: 14px;
+	}
+
+	form input:focus {
+		border: none;
+		outline: none;
+	}
+
+	form label:focus-within {
+		border-color: blue;
+	}
+
+	form button {
+		background: navy;
 		color: white;
 		border: none;
+		padding: 14px 0;
 		border-radius: 5px;
-		padding: 10px 0px;
-		margin-top: 10px;
 		cursor: pointer;
 		font-size: 1rem;
+		display: grid;
+		place-items: center;
 	}
+
 	form button:hover {
 		background: blue;
+	}
+
+	.above,
+	.center {
+		position: absolute;
+		transform: translateY(-50%);
+		pointer-events: none;
+		color: white;
+		border-radius: 4px;
+		padding: 0 6px;
+		font-size: 0.8rem;
+	}
+
+	.above {
+		top: 0;
+		left: 24px;
+		background: navy;
+		border: 1px solid blue;
+		font-size: 0.7rem;
+	}
+
+	.center {
+		top: 50%;
+		left: 6px;
+		border: 1px solid transparent;
+		opacity: 0;
+	}
+
+	.error {
+		color: coral;
+		font-size: 0.9rem;
+		text-align: center;
+	}
+
+	.options {
+		padding: 14px 0;
+		overflow: hidden;
+		font-size: 0.9rem;
+		display: flex;
+		flex-direction: column;
+		gap: 4px;
+	}
+
+	.options > p {
+		position: relative;
+		text-align: center;
+		width: fit-content;
+		margin: 0 auto;
+		padding: 0 8px;
+	}
+
+	.options > p::after,
+	.options > p::before {
+		position: absolute;
+		content: '';
+		top: 50%;
+		transform: translateY(-50%);
+		width: 100vw;
+		height: 1.5px;
+		background: white;
+	}
+
+	.options > p::after {
+		right: 100%;
+	}
+
+	.options > p::before {
+		left: 100%;
+	}
+
+	.options div {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+		justify-content: center;
+	}
+
+	.options div p:last-of-type {
+		color: cyan;
+		cursor: pointer;
+	}
+
+	.loadingSpinner {
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
 	}
 </style>
