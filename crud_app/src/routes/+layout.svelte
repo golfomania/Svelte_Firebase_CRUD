816ea6e71814b24da82ec4e7fe235449757d15ea
golfomania <ml.loeffler@gmx.de>
2024-01-20 20:50:06 +0100

    up to 1h

--- crud_app/src/routes/+layout.svelte
+++ crud_app/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
 	import { auth, db } from '../lib/firebase/firebase.js';
+	import { doc, getDoc, setDoc } from 'firebase/firestore';
+	import { get } from 'svelte/store';
 
 	const nonAuthRoutes = ['/', '/product'];
 	onMount(() => {
@@ -15,6 +17,22 @@
 
 			if (user && currentPath === '/') {
 				window.location.href = '/dashboard';
+				return;
+			}
+
+			const docRef = doc(db, 'users', user.uid);
+			const docSnap = await getDoc(docRef);
+
+			if (!docSnap.exists()) {
+				userRef = doc(db, 'users', user.uid);
+				await setDoc(
+					userRef,
+					{
+						email: user.email,
+						todos: []
+					},
+					{ merge: true }
+				);
 				return;
 			}
 		});
