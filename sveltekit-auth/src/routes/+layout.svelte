f7158867b26dfbaa5b895a517a29fcacf20824b1
golfomania <ml.loeffler@gmx.de>
2024-02-13 14:47:39 +0100

    working on firebase auth

--- sveltekit-auth/src/routes/+layout.svelte
+++ sveltekit-auth/src/routes/+layout.svelte
@@ -1,1 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	import { auth } from '$lib/firebase/auth.js';
+
+	onMount(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			console.log(user);
+			if (user) {
+				console.log('User is signed in');
+			} else {
+				console.log('User is signed out');
+			}
+		});
+	});
+</script>
+
 <slot />
---
+++ sveltekit-auth/src/stores/authStore.js
@@ -0,0 +1,28 @@
+import {
+	createUserWithEmailAndPassword,
+	signOut,
+	sendPasswordResetEmail,
+	updateEmail
+} from 'firebase/auth';
+import { writable } from 'svelte/store';
+import { auth } from '$lib/firebase/firebase.client.js';
+
+export const authStore = writable({
+	isLoading: true,
+	currentUser: null
+});
+
+export const authHandlers = {
+	signup: async (email, password) => {
+		await createUserWithEmailAndPassword(auth, email, password);
+	},
+	logout: async () => {
+		await signOut(auth);
+	},
+	resetPassword: async (email) => {
+		await sendPasswordResetEmail(auth, email);
+	},
+	updateEmail: async (email) => {
+		await updateEmail(auth, email);
+	}
+};
