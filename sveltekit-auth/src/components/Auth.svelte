c5bbfcfcbeb8ca8faad36d34780f0f66b8e187a3
golfomania <ml.loeffler@gmx.de>
2024-02-25 19:27:16 +0100

    added a bit

--- sveltekit-auth/src/components/Auth.svelte
+++ sveltekit-auth/src/components/Auth.svelte
@@ -1,13 +1,29 @@
+<script>
+	let register = true;
+	let email = '';
+	let password = '';
+	let confirmPassword = '';
+</script>
+
 <div class="container">
-	<h1>Sign up</h1>
+	<h1>{register ? 'Sign up' : 'Sign in'}</h1>
 	<form action="">
 		<label for="">
-			<input type="text" placeholder="email" />
+			<input bind:value={email} type="text" placeholder="email" />
 		</label>
 		<label for="">
-			<input type="password" placeholder="password" />
+			<input bind:value={password} type="password" placeholder="password" />
 		</label>
+		{#if register}
+			<label for="">
+				<input bind:value={confirmPassword} type="password" placeholder="confirm password" />
+			</label>
+		{/if}
 		<button type="submit">Sign up</button>
+		<p class="switchHintText">{register ? 'Already have an account?' : "Don't have an account?"}</p>
+		<button type="button" on:click={() => (register = !register)}>
+			{register ? 'I want to sign in' : ' I want to sign up'}
+		</button>
 	</form>
 </div>
 
@@ -23,5 +39,13 @@
 	.container form {
 		display: flex;
 		flex-direction: column;
+	}
+
+	.container form label input {
+		width: 100%;
+	}
+
+	.switchHintText {
+		margin-top: 3rem;
 	}
 </style>
